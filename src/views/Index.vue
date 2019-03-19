d097a3ee78b987aa8e4d3972bc242e93a1ea856e
tianxingxc <tianxingxc@163.com>
2019-03-19 18:49:16 +0800

    radius web init

---
+++ src/views/Index.vue
@@ -0,0 +1,3 @@
+<template>
+    <p>搞笑啊</p>
+</template>
---
+++ src/views/login/Login.vue
@@ -0,0 +1,49 @@
+<template>
+<div class="panel-wrapper">
+  <div class="panel">
+    <!--面板的头部-->
+    <div class="panel-top">
+        <h2>登陆页面</h2>
+    </div>
+    <form id="theForm" action="/login" method="post">
+        <!--面板的主要内容-->
+        <div class="panel-content">
+            <div class="int">
+                <label><img src="../../assets/images/user.png" width="20px" height="20px"></label>
+                <input type="text" name="username" id="username" placeholder="你的手机号或者用户名" />
+            </div>
+            <div class="int">
+                <label><img src="../../assets/images/passwd.png" width="20px" height="20px"></label>
+                <input type="password" name="password" id="password" placeholder="请输入密码" />
+            </div>
+            <div class="login">
+                <button type="button" @click="onJumpIndex()">登录</button>
+            </div>
+            <div class="reg">
+                <span style="font-weight: 500;color: orangered"></span>
+            </div>
+        </div>
+    </form>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data() {
+      return {
+          
+      }
+  },
+  methods: {
+      onJumpIndex() {
+          this.$router.push('/index')
+      }
+  }
+}
+</script>
+
+<style scoped>
+@import "./login.css";
+</style>
