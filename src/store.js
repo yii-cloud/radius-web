bb2daf1cc44c9e58bd8214e6c64b764b28209d8f
tianxingxc <tianxingxc@163.com>
2019-03-27 18:15:39 +0800

    components

--- src/store.js
+++ src/store.js
@@ -3,29 +3,6 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
-const state = {
-  userName: "",
-  realName: ""
-};
-
-const mutations = {
-  setUser(user) {
-    state.userName = user.userName;
-    state.realName = user.realName;
-  }
-};
-
-const getters = {
-  getUser() {
-    return {
-      userName: state.userName,
-      realName: state.realName
-    };
-  }
-};
-
 export default new Vuex.Store({
-  state,
-  mutations,
-  getters
+
 });
